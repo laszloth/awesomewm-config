180a5c0b31cb5ff6cedbf41bdfc918e5b96856d5
Laszlo Toth <laszlth@gmail.com>
2017-10-03 23:09:43 +0200

    added desktop and sessions files

---
+++ scripts/install_awesome_with_gnome.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+cp -v ./files/55awesome-javaworkaround /etc/X11/Xsession.d/
+cp -v ./files/awesome.desktop /usr/share/applications/
+cp -v ./files/awesome-xsessions.desktop /usr/share/xsessions/awesome.desktop
+cp -v ./files/awesome.session /usr/share/gnome-session/sessions/
+cp -v ./files/10-awesome.conf /etc/lightdm/lightdm.conf.d/
