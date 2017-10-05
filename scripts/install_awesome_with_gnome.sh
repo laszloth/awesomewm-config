f4a476e58f42c4b36de09bd0524bcf40f7670c86
Laszlo Toth <laszlth@gmail.com>
2017-10-06 01:30:46 +0200

    added acpi to install script

--- scripts/install_awesome_with_gnome.sh
+++ scripts/install_awesome_with_gnome.sh
@@ -5,3 +5,5 @@ cp -v ./files/awesome.desktop /usr/share/applications/
 cp -v ./files/awesome-xsessions.desktop /usr/share/xsessions/awesome.desktop
 cp -v ./files/awesome.session /usr/share/gnome-session/sessions/
 cp -v ./files/10-awesome.conf /etc/lightdm/lightdm.conf.d/
+
+cp -rv ./files/acpi/ /etc/
