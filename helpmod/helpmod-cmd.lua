e6d553a1f1dcd4e439e16abe85777a63fef5ec5e
Laszlo Toth <laszlth@gmail.com>
2017-10-20 21:07:38 +0200

    fix piped commands' return value

--- helpmod/helpmod-cmd.lua
+++ helpmod/helpmod-cmd.lua
@@ -6,14 +6,14 @@ hcmd.locker = "light-locker-command -l"
 hcmd.calc = "gnome-calculator"
 
 -- get commands
-hcmd.g_jack = [[cat /proc/asound/card1/codec#0 | grep 'Pin-ctls:' | head -3 | tail -1 | grep -c OUT]]
-hcmd.g_ismuted = [[pactl list sinks | grep "^\s\+Mute: yes" | awk '{print $2}']]
+hcmd.g_jack = [[cat /proc/asound/card1/codec#0 | grep 'Pin-ctls:' | head -3 | tail -1 | grep -c OUT; exit ${PIPESTATUS[0]}]]
+hcmd.g_ismuted = [[pactl list sinks | grep "^\s\+Mute: yes" | awk '{print $2}'; exit ${PIPESTATUS[0]}]]
 hcmd.g_aconline = [[cat /sys/class/power_supply/AC/online]]
 hcmd.g_onlaptop = [[laptop-detect; echo $?]]
 hcmd.g_corecnt = [[awk '/cpu cores/ {print $4; exit;}' /proc/cpuinfo]]
 
 -- raw commands
-local _volume = [[pactl list sinks | grep "^\s\+Volume" | awk '{print $5}' | tr -d '%']]
+local _volume = [[pactl list sinks | grep "^\s\+Volume" | awk '{print $5}' | tr -d '%'; exit ${PIPESTATUS[0]}]]
 local _backlight = [[xbacklight -get]]
 local _battery = [[cat /sys/class/power_supply/BAT0/capacity]]
 local _play = [[~/.config/awesome/scripts/XF86Play.sh >/dev/null 2>&1]]
@@ -28,7 +28,7 @@ local bl_step = 7 --percent
 local vol_step = 2 --percent
 
 -- get commands w/ shell
-hcmd.g_volume = {"sh", "-c", _volume}
+hcmd.g_volume = {"bash", "-c", _volume}
 hcmd.g_backlight = {"sh", "-c", _backlight}
 hcmd.g_battery = {"sh", "-c", _battery}
 hcmd.g_mpstatus = {"sh", "-c", _mpstatus}
@@ -40,9 +40,9 @@ hcmd.s_prev = {"sh", "-c", _prev}
 hcmd.s_toggletp = {"sh", "-c", _toggletp}
 
 -- set-get commands for syncronization
-hcmd.sg_lowervol = {"sh", "-c", "pactl set-sink-volume 0 -"..vol_step.."% >/dev/null 2>&1;".._volume}
-hcmd.sg_raisevol = {"sh", "-c", "pactl set-sink-volume 0 +"..vol_step.."% >/dev/null 2>&1;".._volume}
-hcmd.sg_togglemute = {"sh", "-c", _togglemute..';'.._volume}
+hcmd.sg_lowervol = {"bash", "-c", "pactl set-sink-volume 0 -"..vol_step.."% >/dev/null 2>&1;".._volume}
+hcmd.sg_raisevol = {"bash", "-c", "pactl set-sink-volume 0 +"..vol_step.."% >/dev/null 2>&1;".._volume}
+hcmd.sg_togglemute = {"bash", "-c", _togglemute..';'.._volume}
 hcmd.sg_brightdown = {"sh", "-c", "xbacklight -dec "..bl_step.." >/dev/null 2>&1;".._backlight}
 hcmd.sg_brightup = {"sh", "-c", "xbacklight -inc "..bl_step.." >/dev/null 2>&1;".._backlight}
 
