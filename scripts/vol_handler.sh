95d77de3b760fef023be46c69ba1c45818de9721
Laszlo Toth <laszlth@gmail.com>
2017-11-28 18:07:47 +0100

    vol_handler.sh: pactl stops at 0 volume

--- scripts/vol_handler.sh
+++ scripts/vol_handler.sh
@@ -8,14 +8,23 @@ SCRIPTDIR=$(dirname $(realpath $0))
 
 read -ra RAWDATA <<< "$($SCRIPTDIR/sound_handler.sh raw)"
 STEP=$2
-# bus type
+# set step according to bus type
 if [ "${RAWDATA[3]}" = "usb" ]; then
     STEP=$3
+fi
+VOLUME=${RAWDATA[2]}
+
+# pactl stops at 0
+temp=$((VOLUME${1}STEP))
+if [ $temp -gt 0 ]; then
+    RAWDATA[2]=$temp
+else
+    RAWDATA[2]=0
 fi
 
 pactl set-sink-volume ${RAWDATA[0]} ${1}${STEP}%
 
-RAWDATA[2]=$((${RAWDATA[2]}${1}${STEP}))
+# "send" results
 echo "${RAWDATA[@]}"
 
 exit 0
